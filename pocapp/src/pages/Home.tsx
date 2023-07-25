9c27d2c9ac273398f3bf842614b006cd55ddade7
Matthieu Burban <matthieuburban@mbp-de-matthieu.home>
2023-07-25 23:35:47 +0200

    feat: animation of the home page

--- pocapp/src/pages/Home.tsx
+++ pocapp/src/pages/Home.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { Fade } from "react-awesome-reveal";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+
+
 
 export default function Home(){
     
     return (
         <div className="App">
             <header className="App-header">
-            <h2>Test de plusieurs type de scrutins</h2>
-            <Button variant="primary" href="/scrutinMaj"> Start the experiment </Button>
+                <Fade delay={2e2} duration={2e3} cascade> 
+                    <h2>Test de plusieurs type de scrutins</h2>
+                    <Button variant="primary" href="/scrutinMaj"> Start the experiment </Button>
+                </Fade>
             </header>
         </div>
        

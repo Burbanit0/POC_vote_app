e2b5a38bafea72de2c5d906538fed9ea6229347d
Matthieu Burban <matthieuburban@mbp-de-matthieu.home>
2023-07-25 23:35:05 +0200

    fix: style of the drag/drop list

--- pocapp/src/pages/ScrutinClass.tsx
+++ pocapp/src/pages/ScrutinClass.tsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
-import { ListGroup} from "react-bootstrap";
+import { Col, Container, ListGroup, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import foodsJson from "../libs/foods.json";
 import { Food } from "./ScrutinMajoritaire";
 import userService from "../services/user.service";
 import IUser from "../types/user.type";
 
-// const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-// 	padding: 10,
-// 	margin: `0 50px 15px 50px`,
-// 	background: isDragging ? "blue" : "white",
-// 	color: isDragging ? "white" : "black",
-// 	border: `1px solid black`,
-// 	fontSize: `20px`,
-// 	borderRadius: `5px`,
-
-// 	...draggableStyle
-// })
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+	padding: 2,
+	margin: `0 2px 2px 2px`,
+	background: isDragging ? "grey" : "white",
+	color: isDragging ? "white" : "black",
+	border: `1px solid black`,
+	fontSize: `20px`,
+	borderRadius: `5px`,
+	...draggableStyle
+})
 
 
 function SaveList(foods:Array<Food>, user:IUser | undefined){
@@ -51,38 +50,49 @@ export default function ScrutinClass(props:ScrutinType){
 	}
 
     return (
-        <>
-        <h2>Ordonnez les plats selon vos préférences.</h2>
-			<DragDropContext onDragEnd={onDragEnd}>
-				<Droppable droppableId="foods">
-					{(provided) => (
-					<ListGroup>
-						<div className="foods" {...provided.droppableProps} ref={provided.innerRef}>
-							{foods.map((food, index) => {
-								return (
-									<ListGroup.Item className="my-2">
-										<Draggable key={food.id.toString()} draggableId={food.id.toString()} index={index}>
-											{(provided, snapshot) => (
-												<div
-													ref={provided.innerRef}
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													// style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-													>
-													{food.name}
-												</div>
-											)}
-										</Draggable>
-									</ListGroup.Item>
-								)
-							})}
-							</div>
-						</ListGroup>
-					)}
-				</Droppable>
-			</DragDropContext>
-
-			<Button href="/scrutinW" onClick={()=> SaveList(foods, props.user)}>Save List</Button>
-        </>
+        <><Container>
+			<Row className="justify-content-md-center">
+				<Col xs={6}>
+					<h2>Ordonnez les plats selon vos préférences.</h2>
+				</Col>
+			</Row>
+			<Row>
+				<DragDropContext onDragEnd={onDragEnd}>
+					<Droppable droppableId="foods">
+						{(provided) => (
+							<Row className="justify-content-md-center" md={4}>
+								<Col xs={4}>
+									<div className="foods" {...provided.droppableProps} ref={provided.innerRef}>
+										{foods.map((food, index) => {
+											return (
+												<Draggable key={food.id.toString()} draggableId={food.id.toString()} index={index}>
+													{(provided, snapshot) => (
+														<div
+															ref={provided.innerRef}
+															{...provided.draggableProps}
+															{...provided.dragHandleProps}
+															style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+														>
+															{food.name}
+														</div>
+													)}
+												</Draggable>
+											);
+										})}
+									</div>
+								</Col>
+							</Row>
+						)}
+					</Droppable>
+				</DragDropContext>
+			</Row>
+		</Container>
+		<Container>
+				<Row className="justify-content-md-center">
+					<Col md="auto">
+						<Button href="/scrutinW" onClick={() => SaveList(foods, props.user)}>Save List</Button>
+					</Col>
+				</Row>
+			</Container></>
     )
 }

6cd43ed203412ceaa7faee6a18aab7b125ef9167
Matthieu Burban <matthieuburban@mbp-de-matthieu.home>
2023-08-05 20:17:06 +0200

    fix: corrections

--- jwt_auth/src/middleware/deserializeUser.ts
+++ jwt_auth/src/middleware/deserializeUser.ts
@@ -26,7 +26,10 @@ export const deserializeUser = async (
     }
 
     // Validate Access Token
-    const decoded = verifyJwt<{ sub: string }>(access_token);
+    const decoded = verifyJwt<{ sub: string }>(
+      access_token,
+      'accessTokenPublicKey'
+    );
 
     if (!decoded) {
       return next(new AppError(`Invalid token or user doesn't exist`, 401));
